c888ef04790a3e2ef9f4e50d9820dc48a0687193
Farouk HADJ NACER <73796490+Farouk96@users.noreply.github.com>
2022-09-26 13:51:04 +0100

    Update densenet.py

--- densenet.py
+++ densenet.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 import torchvision.models as models
 
 class DenseNet(nn.Module):
-    def __init__(self, dataset, pretrained=True):
+    def __init__(self, pretrained=True):
         super(DenseNet, self).__init__()
-        num_classes = 50 if dataset=="ESC" else 10
+        num_classes = 6
         self.model = models.densenet201(pretrained=pretrained)
         self.model.classifier = nn.Linear(1920, num_classes)
         
