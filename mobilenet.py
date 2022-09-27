9e57e3d59d6c808dff5524360596c77a081bd2a9
Farouk HADJ NACER <73796490+Farouk96@users.noreply.github.com>
2022-09-27 12:43:23 +0100

    Update mobilenet.py

--- mobilenet.py
+++ mobilenet.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torchvision.models as models
 
 class MobileNet(nn.Module):
-    def __init__(self, dataset, pretrained=True):
+    def __init__(self,  pretrained=True):
 		    super(MobileNet, self).__init__()
 		    num_classes = 6
 		    self.model = models.mobilenet_v3_small(pretrained=pretrained)
